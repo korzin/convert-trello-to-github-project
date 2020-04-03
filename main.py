063f0e08349d6cccd4b5d60b5bb188eb36d78a20
korzin <Samkorzin@gmail.com>
2020-04-03 23:46:00 +0300

    with_auth_params

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python
 
+import copy
+import json
 import requests
 import sys
-import json
 
 trello_basic_path = "https://api.trello.com"
 
-def main():
-    api_key = sys.argv[1]
-    auth_token = sys.argv[2]
 
+def main():
     url = trello_basic_path + "/1/members/me/boards"
     headers = {"Accept": "application/json"}
-    query = {
-        'key': api_key, 'token': auth_token
-    }
+    query = {}
+    params = with_auth_params(query)
 
     response = requests.request(
         "GET",
         url,
         headers=headers,
-        params=query
+        params=params
     )
 
     print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+
+def with_auth_params(params):
+    params_with_auth = copy.deepcopy(params)
+    api_key = sys.argv[1]
+    auth_token = sys.argv[2]
+    params_with_auth['key'] = api_key
+    params_with_auth['token'] = auth_token
+    return params_with_auth
 
 if __name__ == '__main__':
     main()
