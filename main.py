ee5bce03d302144addc228fcddd3cb9c90f6c9f0
korzin <Samkorzin@gmail.com>
2020-04-04 23:57:11 +0300

    fetch cards by lists

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 
 import copy
 import json
+import re
 import requests
 import sys
-import re
 
 # class RestRespValidationException(Exception):
 #     pass
@@ -21,8 +21,30 @@ FLOW_LIST_SCHEME = ["todo", "in progress", "done"]  # will be dynamic
 def main():
     source_board_id = get_board_id_by_name(ARG_SOURCE_BOARD_NAME)
     all_source_lists = get_all_lists_by_board(source_board_id)
-    lists_by_name = extract_as_lists_flow_scheme(all_source_lists)
-    print("lists to export : " + str(lists_by_name))
+    lists_scheme = extract_as_lists_flow_scheme(all_source_lists)
+    list_to_cards = get_lists_with_cards(lists_scheme)
+    print("print dictionary: lists to casds. \n" + str(list_to_cards))
+
+def get_lists_with_cards(listsByPattern):
+    ordered_lists = []
+    for key, lists in listsByPattern.items():
+        ordered_lists.extend(lists)
+    list_name_to_cards = {}
+    for list in ordered_lists:
+        url = TRELLO_BASIC_PATH + "/1/lists/" + list['id'] + "/cards"
+        headers = {"Accept": "application/json"}
+        query = {}
+        params = with_auth_params(query)
+
+        response = requests.request(
+            "GET",
+            url,
+            headers=headers,
+            params=params
+        )
+        list_name_to_cards[list['name']] = json.loads(response.text)
+    return list_name_to_cards
+
 
 def extract_as_lists_flow_scheme(board_lists):
     export_lists_dictionary = {}
@@ -36,6 +58,7 @@ def extract_as_lists_flow_scheme(board_lists):
                 break
 
     return export_lists_dictionary
+
 
 def get_all_lists_by_board(board_id):
     url = TRELLO_BASIC_PATH + "/1/boards/" + board_id + "/lists"
