29d1fc821439cf43b22e06fc57ebd57dfd073d02
korzin <Samkorzin@gmail.com>
2020-04-05 20:40:56 +0300

    run script template

---
+++ run.sh
@@ -0,0 +1,8 @@
+_1_API_KEY="placeholder"
+_2_AUTH_TOKEN="placeholder"
+_3_SOURCE_BOARD_NAME="placeholder"
+_4_GITHUB_LOGIN="placeholder"
+_5_GITHUB_REPO="placeholder"
+_6_GITHUB_TOKEN="placeholder"
+
+python3 main.py $_1_API_KEY $_2_AUTH_TOKEN "$_3_SOURCE_BOARD_NAME" $_4_GITHUB_LOGIN $_5_GITHUB_REPO $_6_GITHUB_TOKEN
